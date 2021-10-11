5409c95cceb64cf269c9d32ffce72bb3f343fa0d
ian balijawa <ianbalijawa16@gmail.com>
2021-10-11 22:45:58 +0300

    create mulitple-type Faqs components

--- src/pages/SaaSProductLandingPage.js
+++ src/pages/SaaSProductLandingPage.js
@@ -1,22 +1,22 @@
-import React from "react";
-import tw from "twin.macro";
-import { css } from "styled-components/macro"; //eslint-disable-line
-import AnimationRevealPage from "helpers/AnimationRevealPage.js";
-import Hero from "components/hero/TwoColumnWithInput.js";
-import Features from "components/features/ThreeColWithSideImage.js";
-import MainFeature from "components/features/TwoColWithButton.js";
-import MainFeature2 from "components/features/TwoColWithTwoHorizontalFeaturesAndButton.js";
-import FeatureWithSteps from "components/features/TwoColWithSteps.js";
-import Pricing from "components/pricing/ThreePlans.js";
-import Testimonial from "components/testimonials/TwoColumnWithImageAndRating.js";
-import FAQ from "components/faqs/SingleCol.js";
-import GetStarted from "components/cta/GetStarted";
-import Footer from "components/footers/FiveColumnWithBackground.js";
-import heroScreenshotImageSrc from "images/hero-screenshot-1.png";
-import macHeroScreenshotImageSrc from "images/hero-screenshot-2.png";
-import prototypeIllustrationImageSrc from "images/prototype-illustration.svg";
-import { ReactComponent as BriefcaseIcon } from "feather-icons/dist/icons/briefcase.svg";
-import { ReactComponent as MoneyIcon } from "feather-icons/dist/icons/dollar-sign.svg";
+import React from 'react';
+import tw from 'twin.macro';
+import { css } from 'styled-components/macro'; //eslint-disable-line
+import AnimationRevealPage from 'helpers/AnimationRevealPage.js';
+import Hero from 'components/hero/TwoColumnWithInput.js';
+import Features from 'components/features/ThreeColWithSideImage.js';
+import MainFeature from 'components/features/TwoColWithButton.js';
+import MainFeature2 from 'components/features/TwoColWithTwoHorizontalFeaturesAndButton.js';
+import FeatureWithSteps from 'components/features/TwoColWithSteps.js';
+import Pricing from 'components/pricing/ThreePlans.js';
+import Testimonial from 'components/testimonials/TwoColumnWithImageAndRating.js';
+import FAQ from 'components/faqs/SingleCol.js';
+import GetStarted from 'components/cta/GetStarted';
+import Footer from 'components/footers/FiveColumnWithBackground.js';
+import heroScreenshotImageSrc from 'images/hero-screenshot-1.png';
+import macHeroScreenshotImageSrc from 'images/hero-screenshot-2.png';
+import prototypeIllustrationImageSrc from 'images/prototype-illustration.svg';
+import { ReactComponent as BriefcaseIcon } from 'feather-icons/dist/icons/briefcase.svg';
+import { ReactComponent as MoneyIcon } from 'feather-icons/dist/icons/dollar-sign.svg';
 
 export default () => {
   const Subheading = tw.span`uppercase tracking-widest font-bold text-primary-500`;
@@ -55,7 +55,8 @@ export default () => {
         subheading={<Subheading>VALUES</Subheading>}
         heading={
           <>
-            We Always Abide by Our <HighlightedText>Principles.</HighlightedText>
+            We Always Abide by Our{' '}
+            <HighlightedText>Principles.</HighlightedText>
           </>
         }
         imageSrc={prototypeIllustrationImageSrc}
@@ -63,16 +64,18 @@ export default () => {
         features={[
           {
             Icon: MoneyIcon,
-            title: "Affordable",
-            description: "We promise to offer you the best rate we can - at par with the industry standard.",
-            iconContainerCss: tw`bg-green-300 text-green-800`
+            title: 'Affordable',
+            description:
+              'We promise to offer you the best rate we can - at par with the industry standard.',
+            iconContainerCss: tw`bg-green-300 text-green-800`,
           },
           {
             Icon: BriefcaseIcon,
-            title: "Professionalism",
-            description: "We assure you that our templates are designed and created by professional designers.",
-            iconContainerCss: tw`bg-red-300 text-red-800`
-          }
+            title: 'Professionalism',
+            description:
+              'We assure you that our templates are designed and created by professional designers.',
+            iconContainerCss: tw`bg-red-300 text-red-800`,
+          },
         ]}
       />
       <Pricing
@@ -84,27 +87,42 @@ export default () => {
         }
         plans={[
           {
-            name: "Personal",
-            price: "$17.99",
-            duration: "Monthly",
-            mainFeature: "For Individuals",
-            features: ["30 Templates", "7 Landing Pages", "12 Internal Pages", "Basic Assistance"]
+            name: 'Personal',
+            price: '$17.99',
+            duration: 'Monthly',
+            mainFeature: 'For Individuals',
+            features: [
+              '30 Templates',
+              '7 Landing Pages',
+              '12 Internal Pages',
+              'Basic Assistance',
+            ],
           },
           {
-            name: "Business",
-            price: "$37.99",
-            duration: "Monthly",
-            mainFeature: "For Small Businesses",
-            features: ["60 Templates", "15 Landing Pages", "22 Internal Pages", "Priority Assistance"],
-            featured: true
+            name: 'Business',
+            price: '$37.99',
+            duration: 'Monthly',
+            mainFeature: 'For Small Businesses',
+            features: [
+              '60 Templates',
+              '15 Landing Pages',
+              '22 Internal Pages',
+              'Priority Assistance',
+            ],
+            featured: true,
           },
           {
-            name: "Enterprise",
-            price: "$57.99",
-            duration: "Monthly",
-            mainFeature: "For Large Companies",
-            features: ["90 Templates", "27 Landing Pages", "37 Internal Pages", "Personal Assistance"]
-          }
+            name: 'Enterprise',
+            price: '$57.99',
+            duration: 'Monthly',
+            mainFeature: 'For Large Companies',
+            features: [
+              '90 Templates',
+              '27 Landing Pages',
+              '37 Internal Pages',
+              'Personal Assistance',
+            ],
+          },
         ]}
       />
       <Testimonial
@@ -118,23 +136,23 @@ export default () => {
           {
             stars: 5,
             profileImageSrc:
-              "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=3.25&w=512&h=512&q=80",
-            heading: "Amazing User Experience",
+              'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=3.25&w=512&h=512&q=80',
+            heading: 'Amazing User Experience',
             quote:
-              "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.",
-            customerName: "Charlotte Hale",
-            customerTitle: "Director, Delos Inc."
+              'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.',
+            customerName: 'Charlotte Hale',
+            customerTitle: 'Director, Delos Inc.',
           },
           {
             stars: 5,
             profileImageSrc:
-              "https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=512&h=512&q=80",
-            heading: "Love the Developer Experience and Design Principles !",
+              'https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=512&h=512&q=80',
+            heading: 'Love the Developer Experience and Design Principles !',
             quote:
-              "Sinor Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
-            customerName: "Adam Cuppy",
-            customerTitle: "Founder, EventsNYC"
-          }
+              'Sinor Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
+            customerName: 'Adam Cuppy',
+            customerTitle: 'Founder, EventsNYC',
+          },
         ]}
       />
       <FAQ
@@ -146,39 +164,41 @@ export default () => {
         }
         faqs={[
           {
-            question: "Are all the templates easily customizable ?",
+            question: 'Are all the templates easily customizable ?',
             answer:
-              "Yes, they all are. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+              'Yes, they all are. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
           },
           {
-            question: "How long do you usually support an standalone template for ?",
+            question:
+              'How long do you usually support an standalone template for ?',
             answer:
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
           },
           {
-            question: "What kind of payment methods do you accept ?",
+            question: 'What kind of payment methods do you accept ?',
             answer:
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
           },
           {
-            question: "Is there a subscribption service to get the latest templates ?",
+            question:
+              'Is there a subscribption service to get the latest templates ?',
             answer:
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
           },
           {
-            question: "Are the templates compatible with the React ?",
+            question: 'Are the templates compatible with the React ?',
             answer:
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
           },
           {
-            question: "Do you really support Internet Explorer 11 ?",
+            question: 'Do you really support Internet Explorer 11 ?',
             answer:
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-          }
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+          },
         ]}
       />
-      <GetStarted/>
+      <GetStarted />
       <Footer />
     </AnimationRevealPage>
   );
-}
+};
